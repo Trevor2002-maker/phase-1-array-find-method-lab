c507ae2ab93807c24f5d21fb03ec530913b89f14
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2022-06-11 18:57:43 +0300

    commit tree 0840cc89337cf03ac67da8df27f718055f48c1ea

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+let superbowlWin=(record)=>{
+    let answer=record.find((item)=> item.result==="W");
+    if(answer){
+        return answer.year;
+    }else{
+        return undefined;
+    }
+}
